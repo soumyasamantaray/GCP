7e4b2ce19e321cce4e1cc2eabe4b710ff2e6615f
soumyasamantaray <92103711+soumyasamantaray@users.noreply.github.com>
2021-10-15 13:50:07 +0530

    updated on 15th

--- GCP/sample_create_VPC.tf
+++ GCP/sample_create_VPC.tf
@@ -1,7 +1,7 @@
 
 
 resource "google_compute_network" "vpc_network" {
-  project                 = "active-dahlia-323510"
+  project                 = "my-project-name"
   name                    = "vpc-network"
   auto_create_subnetworks = true
   mtu                     = 1460
